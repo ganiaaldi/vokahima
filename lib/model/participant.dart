d26821579c763cf845b2b7fc66ed39130894e475
ganiaaldi <mrgad98@gmail.com>
2021-05-24 16:11:32 +0700

    implement listview member on participant page

---
+++ lib/model/participant.dart
@@ -0,0 +1,148 @@
+class Member {
+  String nameMember;
+  String gradeMember;
+  String classMember;
+  String divisionMember;
+  String positionMember;
+  int votingMember;
+  String imageAsset;
+
+  Member({
+    required this.nameMember,
+    required this.gradeMember,
+    required this.classMember,
+    required this.divisionMember,
+    required this.positionMember,
+    required this.votingMember,
+    required this.imageAsset,
+  });
+}
+
+var memberList = [
+  Member(
+    nameMember: 'Riska Wahyuni',
+    gradeMember: 'Semester 3',
+    classMember: 'Sistem Informasi-1',
+    divisionMember: 'Pendidikan Pengembangan & Rohani',
+    positionMember: 'Sekretaris Umum',
+    votingMember: 1,
+    imageAsset: 'images/icon_book.png'
+  ),
+  Member(
+      nameMember: 'Brahmono',
+      gradeMember: 'Semester 5',
+      classMember: 'Sistem Informasi-4',
+      divisionMember: 'Informasi & Komunikasi',
+      positionMember: 'Sekretaris Umum',
+      votingMember: 1,
+      imageAsset: 'images/icon_camera.png'
+  ),
+  Member(
+      nameMember: 'Nurhayati',
+      gradeMember: 'Semester 5',
+      classMember: 'Sistem Informasi-2',
+      divisionMember: 'Dana & Usaha',
+      positionMember: 'Bendahara',
+      votingMember: 2,
+      imageAsset: 'images/icon_money.png'
+  ),
+  Member(
+      nameMember: 'Agra Ramadhan',
+      gradeMember: 'Semester 5',
+      classMember: 'Sistem Informasi-2',
+      divisionMember: 'Minat & Bakat',
+      positionMember: 'Ketua Divisi',
+      votingMember: 2,
+      imageAsset: 'images/icon_ball.png'
+  ),
+  Member(
+      nameMember: 'Syafira Fadillah',
+      gradeMember: 'Semester 1',
+      classMember: 'Sistem Informasi-2',
+      divisionMember: 'Minat & Bakat',
+      positionMember: 'Anggota',
+      votingMember: 1,
+      imageAsset: 'images/icon_ball.png'
+  ),
+  Member(
+      nameMember: 'Rahmania',
+      gradeMember: 'Semester 3',
+      classMember: 'Sistem Informasi-2',
+      divisionMember: 'Minat & Bakat',
+      positionMember: 'Anggota',
+      votingMember: 2,
+      imageAsset: 'images/icon_ball.png'
+  ),
+  Member(
+      nameMember: 'Joko Jaka',
+      gradeMember: 'Semester 1',
+      classMember: 'Sistem Informasi-2',
+      divisionMember: 'Minat & Bakat',
+      positionMember: 'Anggota',
+      votingMember: 2,
+      imageAsset: 'images/icon_ball.png'
+  ),
+  Member(
+      nameMember: 'Sarah Tini',
+      gradeMember: 'Semester 3',
+      classMember: 'Sistem Informasi-1',
+      divisionMember: 'Pendidikan Pengembangan & Rohani',
+      positionMember: 'Anggota',
+      votingMember: 2,
+      imageAsset: 'images/icon_book.png'
+  ),
+  Member(
+      nameMember: 'James Moriarty',
+      gradeMember: 'Semester 5',
+      classMember: 'Sistem Informasi-2',
+      divisionMember: 'Pendidikan Pengembangan & Rohani',
+      positionMember: 'Anggota',
+      votingMember: 1,
+      imageAsset: 'images/icon_book.png'
+  ),
+  Member(
+      nameMember: 'Panji Guruh',
+      gradeMember: 'Semester 1',
+      classMember: 'Sistem Informasi-1',
+      divisionMember: 'Pendidikan Pengembangan & Rohani',
+      positionMember: 'Anggota',
+      votingMember: 2,
+      imageAsset: 'images/icon_book.png'
+  ),
+  Member(
+      nameMember: 'Arif Kurnia',
+      gradeMember: 'Semester 3',
+      classMember: 'Sistem Informasi-4',
+      divisionMember: 'Dana & Usaha',
+      positionMember: 'Ketua Divisi',
+      votingMember: 2,
+      imageAsset: 'images/icon_money.png'
+  ),
+  Member(
+      nameMember: 'Fahmi Miriu',
+      gradeMember: 'Semester 1',
+      classMember: 'Sistem Informasi-5',
+      divisionMember: 'Dana & Usaha',
+      positionMember: 'Anggota',
+      votingMember: 2,
+      imageAsset: 'images/icon_money.png'
+  ),
+  Member(
+      nameMember: 'Nana Ninu',
+      gradeMember: 'Semester 1',
+      classMember: 'Sistem Informasi-2',
+      divisionMember: 'Dana & Usaha',
+      positionMember: 'Anggota',
+      votingMember: 2,
+      imageAsset: 'images/icon_money.png'
+  ),
+  Member(
+      nameMember: 'Nadheera',
+      gradeMember: 'Semester 5',
+      classMember: 'Sistem Informasi-2',
+      divisionMember: 'Informasi & Komunikasi',
+      positionMember: 'Ketua Divisi',
+      votingMember: 2,
+      imageAsset: 'images/icon_camera.png'
+  ),
+];
--- lib/participant_page.dart
+++ lib/participant_page.dart
@@ -1,46 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:vokahima/first_page.dart';
+import 'package:vokahima/model/participant.dart';
+
+var nameText = TextStyle(
+    fontFamily: 'OpenSans',
+    fontWeight: FontWeight.bold,
+    fontSize: 16,
+    color: Colors.black);
 
 class ParticipantPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Partisipan',
-          style: TextStyle(fontFamily: 'OpenSans', fontWeight: FontWeight.bold, fontSize: 16, color: Colors.white),
-        ),
-        ),
-      body:
-      Card(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
-              flex: 1,
-              child: Image.asset('images/human_board.png'),
-            ),
-            Expanded(
-              flex: 2,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Text(
-                      'Farm House Lembang',
-                      style: TextStyle(fontSize: 16.0),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text('Lembang'),
-                  ],
-                ),
-              ),
-            ),
-          ],
+        title: Text(
+          'Partisipan Voting',
+          style: TextStyle(
+              fontFamily: 'OpenSans',
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+              color: Colors.white),
         ),
       ),
-      );
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final Member member = memberList[index];
+          return InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                //return DetailScreen(member: member);
+                return FirstPage();
+              }));
+            },
+            child: Card(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Image.asset(member.imageAsset),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            member.nameMember,
+                            style: nameText,
+                          ),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          Text(member.divisionMember,
+                            style: TextStyle(
+                              decoration: TextDecoration.underline)),
+                          Text(member.positionMember),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+        itemCount: memberList.length,
+      ),
+    );
   }
 }
